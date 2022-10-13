2b91a7e2a38be22cf3ea00d38b9a40af102bc682
joeperpetua <qwerty.perpetua@gmail.com>
2022-10-13 02:25:42 +0200

    adding resources

--- src/Components/Resources.js
+++ src/Components/Resources.js
@@ -5,7 +5,7 @@ function Resources() {
     <div className="Resources">
       <span className='app-anchor' id="resources"></span>
       <h2 className='app-title'>My Resources</h2>
-      
+      <p>All my resources (CV, Certifications) are available in the following <a href='https://github.com/joeperpetua/joeperpetua.github.io/tree/main/src/res' rel="noreferrer" target="_blank">link</a></p>
     </div>
   );
 }
