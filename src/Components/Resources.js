ea20a1cba18d6d701eef2354b036b5c3c229d34e
joeperpetua <qwerty.perpetua@gmail.com>
2022-10-13 22:49:52 +0200

    added attributions

--- src/Components/Resources.js
+++ src/Components/Resources.js
@@ -8,6 +8,11 @@ function Resources() {
       <p className='res-desc'>All my resources (CV, Certifications) are available in the following <a href='https://github.com/joeperpetua/joeperpetua.github.io/tree/main/src/Resources' rel="noreferrer" target="_blank">link</a>.</p>
       <p className='res-desc'><a href='https://github.com/joeperpetua' rel="noreferrer" target="_blank">Github</a></p>
       <p className='res-desc'><a href='https://www.linkedin.com/in/joelperpetua' rel="noreferrer" target="_blank">LinkedIn</a></p>
+      <br></br>
+      <p className='res-desc'>Attributions:</p>
+      <a rel="noreferrer" target="_blank" href="https://icons8.com/icon/3096/menu">Menu icon by Icons8</a>
+      <br></br>
+      <a rel="noreferrer" target="_blank" href="https://icons8.com/icon/fuaPZ2MgTeTk/x">X icon by Icons8</a>
     </div>
   );
 }
