38389cb62d933417494351142a36a2e6ae7d68b0
joeperpetua <qwerty.perpetua@gmail.com>
2023-11-07 12:03:53 +0100

    Fixed typo

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,8 +5,8 @@ function About() {
     <div className="About">
       <span className='app-anchor' id="about"></span>
       <h2 className='app-title'>About me</h2>
-      <p className='about-desc'>Hey! I am Joel Perpetua. I am 23 years old and I was born and raised in Buenos Aires, Argentina.</p>
-      <p className='about-desc'>I am currently based in Paris, France. Despite my development background I started working at Synology France as a Technical Support for NAS devices, at the moment, I am transitioning to software development within the company working on internal tools that interact with the support system for a faster and omptimized workflow for several subsidiaries around the world.</p>
+      <p className='about-desc'>Hello! I am Joel Perpetua. I am 23 years old and I was born and raised in Buenos Aires, Argentina.</p>
+      <p className='about-desc'>I am currently based in Paris, France. Despite my development background I started working at Synology France as a Technical Support for NAS devices. At the moment, I am transitioning to software development within the company working on internal tools that interact with the support system for a faster and optimized workflow for several subsidiaries around the world.</p>
       <p className='about-desc'>On my free time, I enjoy developing and working on my personal projects (currently <a href='#multilang'>MultiLang</a>).</p>
       <p className='about-desc'>
       I also enjoy learning languages and getting to know new cultures. I speak Spanish (native level), English (advanced level) and French (intermediate level) fluently. I studied Japanese for two years and I am currently studying German (both elementary level).
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,7 +3,7 @@ import './Header.css';
 function Header() {
   return (
     <div className="Header">
-        <h2 className='header-title'>Hey ✌️, I'm Joel</h2>
+        <h2 className='header-title'>Hey, I'm Joel ✌️</h2>
         <h3 className='header-sub-title'>I enjoy developing and learning new stuff :)</h3>
     </div>
   );
