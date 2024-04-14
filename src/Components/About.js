a72e664f8bdebd36eb4d694eff487c42a56b0ae6
joeperpetua <qwerty.perpetua@gmail.com>
2024-04-14 21:20:16 +0200

    Update about

--- src/Components/About.js
+++ src/Components/About.js
@@ -9,7 +9,7 @@ function About() {
       <p className='about-desc'>I am currently based in Paris, France. Despite my development background I started working at Synology France as a Technical Support for NAS devices. At the moment, I am transitioning to software development within the company working on internal tools that interact with the support system for a faster and optimized workflow for several subsidiaries around the world.</p>
       <p className='about-desc'>On my free time, I enjoy developing and working on my personal projects (currently <a href='#multilang'>MultiLang</a>).</p>
       <p className='about-desc'>
-      I also enjoy learning languages and getting to know new cultures. I speak Spanish (native level), English (advanced level) and French (intermediate level) fluently. I studied Japanese for two years and I am currently studying German (both elementary level).
+      I also enjoy learning languages and getting to know new cultures. I speak Spanish (native level), English (~C1) and French fluently (~B2). I studied Japanese for two years (of which I remember virtually nothing, but it was a fun experience) and I am currently studying German (~B1 level).
       </p>
       <p className='about-desc'>I feel most comfortable working with JavaScript, but I am more than open to work and learn new technologies and languages :)</p>
     </div>
