c8374f33d1f526baa49ccdc3ed5c4848f05ae7fe
joeperpetua <qwerty.perpetua@gmail.com>
2024-05-17 19:04:53 +0200

    Updat about

--- src/Components/About.js
+++ src/Components/About.js
@@ -7,7 +7,7 @@ function About() {
       <h2 className='app-title'>About me</h2>
       <p className='about-desc'>Hello! I am Joel Perpetua. I am 23 years old and I was born and raised in Buenos Aires, Argentina.</p>
       <p className='about-desc'>I am currently based in Paris, France where I am employed as software developer for Synology France working on internal tools that interact with the support ticketing system for a faster and optimized workflow for all the subsidiaries around the world. I also have some experience working with Linux NAS devices on the data protection area and its services APIs.</p>
-      <p className='about-desc'>On my free time, I enjoy developing and working on my personal projects (currently <a href='#multilang'>MultiLang</a>).</p>
+      <p className='about-desc'>On my free time, I enjoy developing and working on my personal projects (currently <a href='#multilang'>MultiLang</a>). Lately I have been pretty obsessed with bouldering so I spend a lot of my free time in the gym.</p>
       <p className='about-desc'>
       I also enjoy learning languages and getting to know new cultures. I speak Spanish (native level), English (~C1) and French fluently (~B2). I studied Japanese for two years (of which I remember virtually nothing, but it was a fun experience) and I am currently studying German (~B1 level).
       </p>
